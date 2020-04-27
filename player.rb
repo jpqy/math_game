d7e26b6a5ea9ceb788a4dc1ed6b6ab40f125ad33
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-04-27 18:31:30 -0400

    implement player class

--- player.rb
+++ player.rb
@@ -1,2 +1,20 @@
 class Player
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def penalize
+    @lives -= 1
+  end
+
+  def dead?
+    return @lives < 0
+  end
+
+  def to_s
+    "#{self.name}: #{@lives}"
+  end
 end
